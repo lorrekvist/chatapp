98b41f713d8b32e87df6246258627d3fe8b41bf5
Andreas Sanz <andreas.sanz@hotmail.com>
2019-09-04 13:09:12 +0200

    instant

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import axios from 'axios'
+import openSocket from 'socket.io-client';
 import { isLoggedIn, getToken } from './AuthHelper';
 import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
 import { withStyles } from '@material-ui/styles';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = theme => ({
+    message: {
+        marginTop: 5,
+        padding: 5
+    },
     container: {
         display: 'flex',
         flexWrap: 'wrap',
     },
     textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        width: 5000
+
     },
     dense: {
-        marginTop: theme.spacing(2),
-    },
 
-}));
+    }
+
+});
+
+var socket;
 
 class Chat extends React.Component{
 
     state = {
         displayName: "",
-        message: ""
+        message: "",
+        messages: []
     }
+
     handleMessageChange = (e) => {
         this.setState({message: e.target.value})
     }
@@ -45,22 +53,56 @@ class Chat extends React.Component{
             data: {
                 message: this.state.message,
             }
-        })
+        });
+
+        socket.emit('chat message', [getToken(), this.state.message]);
     }
 
     componentDidMount() {
         if (!isLoggedIn()) {
             this.props.history.replace('/login')
         } else {
-           //localStorage.getItem
+            this.getMessagesFromDb();
+            socket = openSocket('http://localhost:3002');
+                socket.on('chat message', (msg) => {
+                    this.setState({messages: [...this.state.messages.reverse(), msg].reverse()})
+  });
         }
+    }
+
+    getMessagesFromDb() {
+        axios({
+            method: 'get',
+            url: 'http://localhost:3001/api/messages',
+            headers: {
+                authorization: 'Bearer ' + getToken()
+            }
+        })
+        .then((res) => {
+            this.setState({messages: res.data.reverse() });
+        });
     }
 
     render() {
         const { classes } = this.props;
         return (
             <div>
-                <form onSubmit = {this.handleSubmit}>
+                <p>
+                    {this.state.messages.map(value => 
+                        <Paper className={classes.message}>
+                            <Typography variant="h5" component="h3">
+                                {value.displayName}
+                            </Typography>
+                            <Typography component="p">
+                                {value.message}
+                            </Typography>
+                            <Typography component="small">
+                                {value.createdAt}
+                            </Typography>
+                        </Paper>
+                    )}
+                </p>
+                <form onSubmit = {this.handleSubmit} className={classes.inputForm}>
                 <TextField
                     id="message"
                     label="message"
